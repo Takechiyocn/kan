cdc42b58f0a753588c8c47043fddacd560619221
Takechiyocn <otagao@gmail.com>
2022-12-23 19:15:35 +0800

    cd2022

---
+++ Java/src/main/java/designstructure/structural/struct5facade/CustomerUse.java
@@ -0,0 +1,19 @@
+package designstructure.structural.struct5facade;
+
+/**
+ * @ClassName CustomerUse
+ * @Description
+ * @Author moku
+ * @Date 2022/12/23 18:59
+ * @Version 1.0
+ */
+public class CustomerUse {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        System.out.println("上班：");
+        facade.open();
+
+        System.out.println("下班：");
+        facade.close();
+    }
+}
---
+++ Java/src/main/java/designstructure/structural/struct5facade/bean/WeChat.java
@@ -0,0 +1,18 @@
+package designstructure.structural.struct5facade.bean;
+
+/**
+ * @ClassName WeChat
+ * @Description
+ * @Author moku
+ * @Date 2022/12/23 18:50
+ * @Version 1.0
+ */
+public class WeChat {
+    public static void open() {
+        System.out.println("打开微信");
+    }
+
+    public static void close() {
+        System.out.println("关闭微信");
+    }
+}
